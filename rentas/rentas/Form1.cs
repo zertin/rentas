10f922fe2d6b1ce5b4c0ae2761b0274ee407cc89
zertin <zzelayareyes@yahoo.es>
2018-10-15 00:30:15 -0600

    agregando boton para saludar

--- rentas/rentas/Form1.cs
+++ rentas/rentas/Form1.cs
@@ -16,5 +16,10 @@ namespace rentas
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola mundo");
+        }
     }
 }
